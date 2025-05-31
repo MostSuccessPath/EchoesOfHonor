66f641bbd89cde2304170c6486cb320d49d48efa
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2025-05-31 20:04:03 -0300

    feat(WorldSelection&SinglePlayer)

--- include/SDLfunctions.h
+++ include/SDLfunctions.h
@@ -7,8 +7,10 @@
 
 void initSDL(SDL_Window **window, SDL_Renderer **renderer);
 
-tile_t *loadImage(int numImage, char *path, SDL_Renderer *renderer);
+tile_t* loadImage(int numImage, char *path, SDL_Renderer *renderer);
 
 void defineSize(int numTile, tile_t *tileset, int w, int h);
+
+void createText(char *fontPath, int fontSize, int *color, SDL_Renderer *renderer, char *text);
 
 #endif
